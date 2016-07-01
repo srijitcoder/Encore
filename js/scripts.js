acb1cfc75caab521ebe4831e3bdb5c6be591c125
srijitcoder <srijitsm123@gmail.com>
2016-07-01 21:23:02 +0530

    Encore Test

---
+++ js/scripts.js
@@ -0,0 +1,156 @@
+/***************** glass ******************/
+
+
+
+/*Srijit*/
+$(document).ready(function() {
+	$("#sri").mouseover(function(){
+        document.getElementById('sri').src="images/sri2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#sri").mouseout(function(){
+        document.getElementById('sri').src="images/sri.jpg";
+    });
+});
+
+
+/*Shivam*/
+$(document).ready(function() {
+	$("#sv").mouseover(function(){
+        document.getElementById('sv').src="images/sv2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#sv").mouseout(function(){
+        document.getElementById('sv').src="images/sv.jpg";
+    });
+});
+
+
+/*Saikutty*/
+$(document).ready(function() {
+	$("#sai").mouseover(function(){
+        document.getElementById('sai').src="images/sai2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#sai").mouseout(function(){
+        document.getElementById('sai').src="images/sai.jpg";
+    });
+});
+
+
+/*yash*/
+$(document).ready(function() {
+	$("#yash").mouseover(function(){
+        document.getElementById('yash').src="images/yash2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#yash").mouseout(function(){
+        document.getElementById('yash').src="images/yash.jpg";
+    });
+});
+
+
+/*prateek*/
+$(document).ready(function() {
+	$("#pra").mouseover(function(){
+        document.getElementById('pra').src="images/pra2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#pra").mouseout(function(){
+        document.getElementById('pra').src="images/pra.jpg";
+    });
+});
+
+
+/*pranay*/
+$(document).ready(function() {
+	$("#pranay").mouseover(function(){
+        document.getElementById('pranay').src="images/pranay2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#pranay").mouseout(function(){
+        document.getElementById('pranay').src="images/pranay.jpg";
+    });
+});
+
+
+/*kartik*/
+$(document).ready(function() {
+	$("#kar").mouseover(function(){
+        document.getElementById('kar').src="images/kar2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#kar").mouseout(function(){
+        document.getElementById('kar').src="images/kar.jpg";
+    });
+});
+
+
+/*anmol*/
+$(document).ready(function() {
+	$("#anm").mouseover(function(){
+        document.getElementById('anm').src="images/anm2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#anm").mouseout(function(){
+        document.getElementById('anm').src="images/anm.jpg";
+    });
+});
+
+
+/*ankur*/
+$(document).ready(function() {
+	$("#ank").mouseover(function(){
+        document.getElementById('ank').src="images/ank2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#ank").mouseout(function(){
+        document.getElementById('ank').src="images/ank.jpg";
+    });
+});
+
+
+/*Saumya*/
+$(document).ready(function() {
+	$("#rawat").mouseover(function(){
+        document.getElementById('rawat').src="images/sau2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#rawat").mouseout(function(){
+        document.getElementById('rawat').src="images/sau.jpg";
+    });
+});
+
+
+/*ankur 2*/
+$(document).ready(function() {
+	$("#anku").mouseover(function(){
+        document.getElementById('anku').src="images/side2.jpg";
+    });
+});
+
+$(document).ready(function() {
+	$("#anku").mouseout(function(){
+        document.getElementById('anku').src="images/side2.jpg";
+    });
+});
